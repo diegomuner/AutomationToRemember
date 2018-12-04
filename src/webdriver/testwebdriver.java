25ed20956f79551d0f65d352b5cd542797d7ebe3
diegomuner <diegomuner@hotmail.com>
2018-12-04 18:29:49 -0300

    minor enhancements

--- src/webdriver/testwebdriver.java
+++ src/webdriver/testwebdriver.java
@@ -1,15 +1,17 @@
 
-
+package webdriver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class testwebdriver {
 	
-	
+	@Test
 	public void test() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\diego\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver browser = new ChromeDriver();
-		browser.get("www.google.com");
+		browser.get("http://www.google.com");
 		try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
